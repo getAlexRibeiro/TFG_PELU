3275c498cf8b34fddae1d17512f2eb8358506a57
RIBEIRO PEROLA ALEXANDRE <alexandre.ribeiro@servexternos.gruposantander.com>
2022-03-24 14:00:55 +0100

    Cambio de carpeta

---
+++ assets/img/servicios.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+  <!-- IMPORTS HEADER BEGIN -->
+<?php include "includes/importHead.php"; ?>
+  <!-- IMPORTS HEADER END -->
+
+ 
+
+  </head>
+  <body>
+
+  <header class="header header-absolute">
+      <!-- Top Bar-->
+      <div class="top-bar">
+        <div class="container-fluid">
+          <div class="row d-flex align-items-center">
+            <div class="col-sm-7 d-none d-sm-block">
+              <ul class="list-inline topbar-text mb-0">
+                <li class="list-inline-item pe-3 me-0">
+                  <svg class="svg-icon me-2">
+                    <use xlink:href="#calls-1"> </use>
+                  </svg>647761117
+                </li>
+                <!--<li class="list-inline-item px-3 border-start d-none d-lg-inline-block">Free shipping on orders over $300</li>-->
+              </ul>
+            </div>
+            <div class="col-sm-5 d-flex justify-content-end">
+              <!-- Language Dropdown-->
+              <div class="dropdown border-end px-3"><a class="dropdown-toggle topbar-link" id="langsDropdown" href="#" data-bs-toggle="dropdown" data-bs-display="static" aria-haspopup="true" aria-expanded="false"><img class="topbar-flag" src="assets/img/flag/gb.svg" alt="english">English</a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated" aria-labelledby="langsDropdown"><a class="dropdown-item text-sm" href="#"><img class="topbar-flag" src="assets/img/flag/de.svg" alt="german">German</a><a class="dropdown-item text-sm" href="#"> <img class="topbar-flag" src="assets/img/flag/fr.svg" alt="french">French</a></div>
+              </div>
+              <!-- Currency Dropdown-->
+              <div class="dropdown ps-3 ms-0"><a class="dropdown-toggle topbar-link" id="currencyDropdown" href="#" data-bs-toggle="dropdown" data-bs-display="static" aria-haspopup="true" aria-expanded="false"> <i class="fas fa-dollar-sign text-xs align-middle mt-n1 me-2"> </i>USD</a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated" aria-labelledby="currencyDropdown"><a class="dropdown-item text-sm" href="#"> <i class="fas fa-euro-sign text-xs align-middle mt-n1 me-2"> </i>EUR</a><a class="dropdown-item text-sm" href="#"><i class="fas fa-pound-sign text-xs align-middle mt-n1 me-2"> </i>GBP</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Top Bar End-->
+       <!-- IMPORTS HEADER BEGIN -->
+<?php include "includes/importMenu.php"; ?>
+  <!-- IMPORTS HEADER END -->
+
+  <section class="py-6">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-8 mb-5">
+            <p class="text-uppercase text-muted fw-bold mb-1">Todos nuestros servicios</p>
+            <p class="lead text-muted">Reserva tu cita solicitando el día y la hora que quieras en la sección de citas, escríbenos por Whastapp o llámanos sin compromiso para consultar cualquier duda que</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 mb-4 mb-md-6 pt-lg-3"><a href="detail-2.html"><img class="img-fluid" src="assets/img/1_Lavar-marcar.jpg" alt=""></a>
+            <div class="px-4 position-relative z-index-2 mt-n3"><a class="text-dark text-decoration-none" href="detail-2.html">
+                <h3>Lavar y Marcar     </h3>
+                <p class="text-muted">15€</p></a>
+              <p> <a class="btn btn-link text-dark text-decoration-none px-0" href="custom-login.php"> Pedir Cita                      </a></p>
+            </div>
+          </div>
+          <div class="col-md-7 ms-auto mb-4 mb-md-5">
+            <div class="position-absolute z-index-5 py-6"><a class="text-dark text-decoration-none" href="detail.html">
+                <h2>Cortar<br>Señoras     </h2>
+                <p class="text-muted">12€</p></a>
+              <p> <a class="btn btn-link text-dark text-decoration-none px-0" href="custom-login.php"> Pedir Cita                      </a></p>
+            </div>
+            <div class="ms-6"><a href="detail.html"><img class="img-fluid" src="assets/img/2-corte-señoras.jpg" alt=""></a></div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-7 mb-4 mb-lg-5">
+            <div class="position-absolute z-index-5 py-6"><a class="text-dark text-decoration-none" href="detail-2.html">
+                <h2>Skeleton<br>Tees<br></h2>
+                <p class="text-muted">$39.90</p></a>
+              <p> <a class="btn btn-link text-dark text-decoration-none px-0" href="detail-2.html"> Shop now                        </a></p>
+            </div>
+            <div class="ms-6"><a href="detail-2.html"><img class="img-fluid" src="img/1_Lavar-marcar.jpg" alt=""></a></div>
+          </div>
+          <div class="col-lg-4 ms-auto pt-lg-6 mb-4 mb-lg-5"><a class="text-dark text-decoration-none" href="detail.html"><img class="img-fluid" src="img/product/artboard-sunglasses.jpg" alt=""></a>
+            <div class="px-4 position-relative z-index-2 mt-n3"><a class="text-dark text-decoration-none" href="detail.html">
+                <h3>Retro<br>sunglasses</h3>
+                <p class="text-muted">$39.90</p></a>
+              <p> <a class="btn btn-link text-dark text-decoration-none px-0"> Shop now     </a></p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 pt-lg-3 mb-4 mb-lg-5"><a href="detail-2.html"><img class="img-fluid" src="img/1_Lavar-marcar.jpg" alt=""></a>
+            <div class="px-4 position-relative z-index-2 mt-n3"><a class="text-dark text-decoration-none" href="detail-2.html">
+                <h3>Swiss<br>All<br>Black     </h3>
+                <p class="text-muted">$149.90</p></a>
+              <p> <a class="btn btn-link text-dark text-decoration-none px-0" href="detail-2.html"> Shop now                       </a></p>
+            </div>
+          </div>
+          <div class="col-lg-8 ms-auto">
+            <div class="position-absolute z-index-5 py-6"><a class="text-dark text-decoration-none" href="detail.html">
+                <h2>Ethnic<br>design<br>everywhere     </h2>
+                <p class="text-muted">$39.90</p></a>
+              <p> <a class="btn btn-link text-dark text-decoration-none px-0" href="detail.html"> Shop now      </a></p>
+            </div>
+            <div class="ms-6"><a href="detail.html"><img class="img-fluid" src="img/1_Lavar-marcar.jpg" alt=""></a></div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  
+
+  <section class="position-relative py-6 mb-4"><img class="bg-image" src="assets/img/1_Lavar-marcar.jpg" alt="">
+      <div class="container position-relative">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="bg-white p-5"><strong class="text-uppercase text-muted d-inline-block mb-2 text-sm">Featured</strong>
+              <h2 class="mb-3">Fashion, purpose & freedom</h2>
+              <p class="text-muted">As am hastily invited settled at limited civilly fortune me. Really spring in extent an by. Judge but built party world. Of so am he remember although required. Bachelor unpacked be advanced at. Confined in declared marianne is vicinity</p><a class="btn btn-link text-dark p-0" href="post.html">Continue reading <i class="fa fa-long-arrow-alt-right"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+
+<!-- IMPORTS FOOTER BEGIN -->
+<?php include "includes/importFooter.php"; ?>
+  <!-- IMPORTS FOOTER END -->
