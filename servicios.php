dfd02858d42a0a94a1d2b197528f01c609f9acab
Vikers <victorperezcolmenarejo03@gmail.com>
2022-03-29 11:24:33 +0200

    Create servicios.php

---
+++ servicios.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+  <!-- IMPORTS HEADER BEGIN -->
+<?php include "includes/importHead.php"; ?>
+  <!-- IMPORTS HEADER END -->
+
+ 
+
+  </head>
+  <body>
+
+  <header class="header header-absolute">
+      <!-- Top Bar-->
+      <div class="top-bar">
+        <div class="container-fluid">
+          <div class="row d-flex align-items-center">
+            <div class="col-sm-7 d-none d-sm-block">
+              <ul class="list-inline topbar-text mb-0">
+                <li class="list-inline-item pe-3 me-0">
+                  <svg class="svg-icon me-2">
+                    <use xlink:href="#calls-1"> </use>
+                  </svg>647761117
+                </li>
+                <!--<li class="list-inline-item px-3 border-start d-none d-lg-inline-block">Free shipping on orders over $300</li>-->
+              </ul>
+            </div>
+            <div class="col-sm-5 d-flex justify-content-end">
+              <!-- Language Dropdown-->
+              <div class="dropdown border-end px-3"><a class="dropdown-toggle topbar-link" id="langsDropdown" href="#" data-bs-toggle="dropdown" data-bs-display="static" aria-haspopup="true" aria-expanded="false"><img class="topbar-flag" src="assets/img/flag/gb.svg" alt="english">English</a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated" aria-labelledby="langsDropdown"><a class="dropdown-item text-sm" href="#"><img class="topbar-flag" src="assets/img/flag/de.svg" alt="german">German</a><a class="dropdown-item text-sm" href="#"> <img class="topbar-flag" src="assets/img/flag/fr.svg" alt="french">French</a></div>
+              </div>
+              <!-- Currency Dropdown-->
+              <div class="dropdown ps-3 ms-0"><a class="dropdown-toggle topbar-link" id="currencyDropdown" href="#" data-bs-toggle="dropdown" data-bs-display="static" aria-haspopup="true" aria-expanded="false"> <i class="fas fa-dollar-sign text-xs align-middle mt-n1 me-2"> </i>USD</a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated" aria-labelledby="currencyDropdown"><a class="dropdown-item text-sm" href="#"> <i class="fas fa-euro-sign text-xs align-middle mt-n1 me-2"> </i>EUR</a><a class="dropdown-item text-sm" href="#"><i class="fas fa-pound-sign text-xs align-middle mt-n1 me-2"> </i>GBP</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Top Bar End-->
+       <!-- IMPORTS HEADER BEGIN -->
+<?php include "includes/importMenu.php"; ?>
+  <!-- IMPORTS HEADER END -->
+
+  <section class="position-relative py-6 mb-4"><img class="bg-image" src="assets/img/photo/matese-fields-233175-unsplash.jpg" alt="">
+      <div class="container position-relative">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="bg-white p-5"><strong class="text-uppercase text-muted d-inline-block mb-2 text-sm">Featured</strong>
+              <h2 class="mb-3">Fashion, purpose & freedom</h2>
+              <p class="text-muted">As am hastily invited settled at limited civilly fortune me. Really spring in extent an by. Judge but built party world. Of so am he remember although required. Bachelor unpacked be advanced at. Confined in declared marianne is vicinity</p><a class="btn btn-link text-dark p-0" href="post.html">Continue reading <i class="fa fa-long-arrow-alt-right"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+     <!-- ABOUT
+    ================================================== -->
+    <section class="section pb-0">
+
+      <!-- Content -->
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 col-lg-5 offset-lg-1">
+            
+            <!-- Image -->
+            <div class="img-effect img-effect-solid mb-5 mb-md-0">
+              <img src="assets/img/tools.jpg" class="img-fluid" alt="...">
+            </div>
+
+          </div>
+          <div class="col-md-6 col-lg-4 offset-lg-1">
+
+            <!-- Heading -->
+            <h2 class="mb-4">
+              Swiss-made engineering excellence
+            </h2>
+
+            <!-- Content -->
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, maiores, aperiam. Excepturi assumenda non quasi ipsa quidem harum debitis aut alias dicta. Laudantium reprehenderit ea, suscipit nulla deleniti excepturi repudiandae!
+            </p>
+
+            <!-- Button -->
+            <a href="#modal-video" data-toggle="modal" class="btn btn-outline-primary btn-circle mr-3">
+              <i class="fas fa-play"></i>
+            </a> Play video
+            
+          </div>
+        </div> <!-- / .row -->
+      </div> <!-- / .container -->
+
+    </section>
+
+    <!-- ABOUT
+    ================================================== -->
+    <section class="section" style="padding-bottom: 40px;">
+
+      <!-- Content -->
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 col-lg-5 offset-lg-1 order-md-2">
+            
+            <!-- Image -->
+            <div class="img-effect img-effect-border mb-5 mb-md-0">
+              <img src="assets/img/tools.jpg" class="img-fluid" alt="...">
+            </div>
+
+          </div>
+          <div class="col-md-6 col-lg-4 offset-lg-1 order-md-1">
+
+            <!-- Heading -->
+            <h2 class="mb-4">
+              Stylish high-quality timepieces
+            </h2>
+
+            <!-- Content -->
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima tenetur, non ea, nobis minus distinctio eveniet libero fugiat, ullam deserunt dolores exercitationem quae eum? Alias quaerat rerum, quasi nulla odit?
+            </p>
+
+            <!-- Button -->
+            <a href="contact.html" class="btn btn-outline-primary">
+              Contact us
+            </a>
+            
+          </div>
+        </div> <!-- / .row -->
+      </div> <!-- / .container -->
+
+    </section>
+
+<!-- IMPORTS FOOTER BEGIN -->
+<?php include "includes/importFooter.php"; ?>
+  <!-- IMPORTS FOOTER END -->
