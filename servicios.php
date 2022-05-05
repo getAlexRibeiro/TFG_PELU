711ea009f832e8e058c1d965afd2dfb9046788b9
Vikers <victorperezcolmenarejo03@gmail.com>
2022-05-05 14:01:57 +0200

    lesgoooo

--- servicios.php
+++ servicios.php
@@ -18,22 +18,34 @@ $datos= mysqli_query ($con,$consulta_servicios);
 <?php include "includes/importMenu.php"; ?>
   <!-- IMPORTS HEADER END -->
 
-
+  <div class="container">
+        <!-- Breadcrumbs -->
+        <ol class="breadcrumb justify-content-center">
+          <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+          <li class="breadcrumb-item active">Servicios</li>
+        </ol>
+        <!-- Hero Content-->
+        <div class="hero-content pb-5 text-center">
+          <h1 class="hero-heading">Servicios</h1>
+          <div class="row">   
+          </div>
+        </div>
+      </div>
     
   <div class="album py-5 bg-light">
     <div class="container ">
 
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <div>
         <div class="row ">
         <?php while($fila=mysqli_fetch_array($datos)) : ?>
-          <div class="card shadow-sm">
-            <img class="img-fluid d-flex justify-content-between align-items-center" src="assets/img/photo/ejemplo_ambos.jpeg" alt="pelo adulto">
+          <div class="card shadow-sm col-md-4 col-sm-4" >
+            <img class="img-fluid d-flex justify-content-between align-items-center" width="100%" height="100%" src="assets/img/photo/ejemplo_ambos.jpeg" alt="pelo adulto">
 
             <div class="card-body">
               <h3><?php echo $fila['name_servicio']; ?></h3>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Pedir cita</font></font></button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="custom-login.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Pedir cita</font></font></a></button>
                 </div>
                 <small class="text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><?php echo $fila['price_servicio']; ?>€</font></font></small>
               </div>
