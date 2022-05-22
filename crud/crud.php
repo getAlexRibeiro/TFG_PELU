06e193d6825cb4b918e8ac5d92372dd361759fb7
getAlexRibeiro <56071078+getAlexRibeiro@users.noreply.github.com>
2022-05-22 21:05:55 +0200

    sesiones

--- crud/crud.php
+++ crud/crud.php
@@ -1,20 +1,11 @@
 
 <?php
-
-    if(!isset($_COOKIE[$cookie_name])) {
-        echo "Cookie named '" . $cookie_name . "' is not set!";
-    } else {
-        echo "Cookie '" . $cookie_name . "' is set!<br>";
-        echo "Value is: " . $_COOKIE[$cookie_name];
-    }
-
-
-// if(empty($_SESSION['ADMIN'])) {
-//    header("Location: ./index.php");
-// } elseif (!empty($_SESSION['ADMIN'])){
-//    
-// }
+ if(!empty($_SESSION['usuario'])) {
+    header("Location: ./index.php");
+ } elseif (empty($_SESSION['usuario'])){
     include '../conexion.php';  
+ }
+    include '../conexion.php';
     //Crear y seleccionar query
     $query = "SELECT * FROM clientes ORDER BY id_cliente asc";
     $usuarios = mysqli_query($con, $query);
--- crud/index.php
+++ crud/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include '../conexion.php'; 
 $errores = '';
 $enviado = true;
@@ -7,11 +8,8 @@ if (isset($_POST['submit_login_admin'])) {
 
   $login_admin = $_POST['login_admin'];
   $login_admin_pass = $_POST['login_admin_pass'];
-
   if (!empty($login_admin)) { //comprabamos nombre login
-
     $login_admin = filter_var($login_admin, FILTER_SANITIZE_STRING); //limpia o verifica que es un texto
-
   } else {
     $errores .= 'Por ingresa un nombre <br />';
     $enviado = false;
@@ -28,18 +26,12 @@ if (isset($_POST['submit_login_admin'])) {
     echo "<script type='text/javascript'>alert('Usuario o contraseña inválido');</script>";
   } else {
     // Comprobamos que el usuario existe
-    $conexion = new mysqli("localhost", "root", "", "peluqueria");
-
-    if ($conexion->connect_errno) {
+    if ($con->connect_errno) {
       die('Lo siento hubo un problema con el servidor');
     } else {
-      $consulta = mysqli_query($conexion, "SELECT * FROM clientes WHERE Nombre = '$login_admin' and password = '$login_admin_pass' and rol = 'admin'");
+      $consulta = mysqli_query($con, "SELECT * FROM clientes WHERE Nombre = '$login_admin' and password = '$login_admin_pass' and rol = 'admin'");
       if ($login_admin = mysqli_fetch_assoc($consulta)) {
-        //session_start();
-        //$_SESSION['ADMIN'] = $_POST['login_admin'];
-        $cookie_name = $_POST['login_admin'];
-        $cookie_value = "ADMIN";
-        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+        $_SESSION['usuario'] = $_POST['login_admin'];
         header("Location: ./crud.php");
       } else {
         echo "<script type='text/javascript'>alert('Usuario o contraseña inválido');</script>";
