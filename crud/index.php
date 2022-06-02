3f481609baebe936cef1b84f242a795598f37474
RIBEIRO PEROLA ALEXANDRE <alexandre.ribeiro@servexternos.gruposantander.com>
2022-06-02 19:35:07 +0200

    logo no

--- crud/index.php
+++ crud/index.php
@@ -92,10 +92,6 @@ if (isset($_POST['submit_login_admin'])) {
         <div class="row">
           <div class="col-sm-6 text-black">
 
-            <div class="px-5 ms-xl-4">
-              <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-              <span class="h1 fw-bold mb-0">Logo</span>
-            </div>
 
             <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
 
